7b37ceaf0f991321f56d735929709855ac4b33f0
Your Name <aheredia.estudio@gmail.com>
2019-04-16 19:32:41 -0500

    ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -5,3 +5,34 @@
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
 ?>
+
+<html>
+    
+<?php
+function mostrartitulo($men)
+{
+  echo "<h1 style=\"text-align:center\">";
+  echo $men;
+  echo "</h1>";
+}
+
+
+
+?>
+    
+    
+<head>
+<title>Problema</title>
+</head>
+<body>
+
+<?php
+
+   mostrartitulo("tITULO AQUI 1");
+    mostrartitulo("tITULO AQUI 2");
+
+
+?>
+
+</body>
+</html>
