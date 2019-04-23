1fe253693ede52a53a76f1d1122022e815498cc1
Your Name <aheredia.estudio@gmail.com>
2019-04-23 17:31:32 -0500

    ejer

--- ejercicio6/index.php
+++ ejercicio6/index.php
@@ -14,20 +14,23 @@ http://buscapalabras.com.ar/palabras-con-las-cinco-vocales.html
 
 $palabra = 'Murcielago';
 
-    $buscaA = substr_count($palabra, 'a') + substr_count($palabra, 'A'); 
-    $buscaE = substr_count($palabra, 'e') + substr_count($palabra, 'E'); 
-    $buscaI = substr_count($palabra, 'i') + substr_count($palabra, 'I'); 
-    $buscaO = substr_count($palabra, 'o') + substr_count($palabra, 'O'); 
-    $buscaU = substr_count($palabra, 'u') + substr_count($palabra, 'U'); 
+    $palA = substr_count($palabra, 'a') + substr_count($palabra, 'A'); 
+    $palE = substr_count($palabra, 'e') + substr_count($palabra, 'E'); 
+    $palI = substr_count($palabra, 'i') + substr_count($palabra, 'I'); 
+    $palO = substr_count($palabra, 'o') + substr_count($palabra, 'O'); 
+    $palU = substr_count($palabra, 'u') + substr_count($palabra, 'U'); 
     $numeroTOTAL = strlen($palabra);
 
 
-    if($buscaA == 1 and $buscaE == 1 and $buscaI == 1 and $buscaO == 1 and $buscaU == 1){
-        echo "La palabra ($palabra) contiene todas las vocales"; 
+    if($palA == 1 and $palE == 1 and $palI == 1 and $palO == 1 and $palU == 1)
+    {
+        echo "La palabra: ($palabra). Contiene todas las vocales"; 
     }else{
-        echo "La palabra ($palabra) NO tiene todas las vocales";
+        echo "La palabra: ($palabra). No tiene todas las vocales";
+
         echo "<br>";
-        echo 'Palabras que contien:'; 
+
+        echo 'Palabras tiene las sigientes vocales:'; 
         echo "<br>";
         echo "Numero de vocales A: $buscaA <br>";
         echo "Numero de vocales E: $buscaE <br>";
@@ -35,7 +38,3 @@ $palabra = 'Murcielago';
         echo "Numero de vocales O: $buscaO <br>";
         echo "Numero de vocales U: $buscaU <br>";
     }
-}
-    echo "<br>";
-    buscaVocales(); 
-    echo "<br>";
